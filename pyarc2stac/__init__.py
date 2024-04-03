d59086ab8e0e06be2d7a9f84b38211a79b3fe4d2
Abdelhak Marouane <am0089@uah.edu>
2024-04-03 11:38:42 -0500

    Add the import of fetch timeseries

--- pyarc2stac/__init__.py
+++ pyarc2stac/__init__.py
@@ -4,12 +4,18 @@ Pyarc2stac is a library used as proxy to convert ARC files to STAC items
 """
 __all__ = [
     "__version__",
-    "convert_to_collection_stac"
+    "convert_to_collection_stac",
+    "fetch_timeseries"
     ]
 
 from pyarc2stac.version import (
     __version__
 )
+
 from pyarc2stac.arc import (
     convert_to_collection_stac
+)
+
+from pyarc2stac.timeseries import (
+    fetch_timeseries
 )
