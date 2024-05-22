83e33d62b07864104eedaef3c43c4b9ddc4dd057
Abdelhak Marouane <am0089@uah.edu>
2024-05-22 09:43:17 -0500

    Add MapServer Support

--- pyarc2stac/utils.py
+++ pyarc2stac/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import requests
-
+from pyproj import Transformer
 
 def get_data(url):
     r = requests.get(url)
@@ -13,3 +13,26 @@ def convert_to_datetime(times_extent):
     return [
         datetime.utcfromtimestamp(time_extent / 1000.0) for time_extent in times_extent
     ]
+
+
+def transform_projection(wkid_source_proj, x, y):
+    """Converts coordinates from EPSG:3857 to EPSG:4326.
+
+    Args:
+        wkid_source_proj : The projection source
+        x (float): The x coordinate (longitude) in EPSG:3857.
+        y (float): The y coordinate (latitude) in EPSG:3857.
+
+    Returns:
+        tuple: A tuple containing the converted coordinates (longitude, latitude) in EPSG:4326.
+    """
+    wkid_destination_proj = "4326"
+    if wkid_source_proj == wkid_destination_proj:
+        return x, y
+
+    transformer = Transformer.from_crs(f"EPSG:{wkid_source_proj}", f"EPSG:{wkid_destination_proj}")
+
+    # Perform the transformation
+    lat, lon = transformer.transform(x, y)
+
+    return [lat, lon]
