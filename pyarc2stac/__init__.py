a7003ffdbc7d0b3453ed62a4ebd3a3f6ef433115
Slesa Adhikari <slesaad@gmail.com>
2025-03-17 17:01:58 -0500

    Update init to include the added classes

--- pyarc2stac/__init__.py
+++ pyarc2stac/__init__.py
@@ -5,13 +5,15 @@ Pyarc2stac is a library used as proxy to convert ARC files to STAC items
 
 __all__ = [
     "__version__",
-    "convert_to_collection_stac",
     "fetch_timeseries",
     "get_legend",
+    "ArcReader",
+    "WMSReader",
 ]
 
 from pyarc2stac.version import __version__
 
-from pyarc2stac.arc import convert_to_collection_stac, get_legend
+from pyarc2stac.ArcReader import ArcReader
+from pyarc2stac.WMSReader import WMSReader
 
 from pyarc2stac.timeseries import fetch_timeseries
