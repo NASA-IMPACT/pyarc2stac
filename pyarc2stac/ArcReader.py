6254c551561403704a7e6cbd6e99a54121b5f105
Kyle Lesinger <kdl0040@uah.edu>
2025-04-23 07:31:40 -0500

    Update ArcReader.py
    replaced quotes from " " (double quotes) to ' ' (single quotes) to align with f-string expectations.

--- pyarc2stac/ArcReader.py
+++ pyarc2stac/ArcReader.py
@@ -219,7 +219,7 @@ class ArcReader:
 
                     # Store WMS metadata in a Link (valid on Collection)
                     link = Link(
-                        target=f"{self.server_url.replace('/rest', '').strip("/")}/WMSServer",
+                        target=f"{self.server_url.replace('/rest', '').strip('/')}/WMSServer",
                         rel="wms",
                         media_type="image/png",
                         title="Visualized through a WMS",
