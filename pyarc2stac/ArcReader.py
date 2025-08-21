b50ee3b80fd7d2d24e8034d93705580ff3433885
Kyle Lesinger <kdl0040@uah.edu>
2025-08-21 15:49:06 -0500

    update ArcReader

--- pyarc2stac/ArcReader.py
+++ pyarc2stac/ArcReader.py
@@ -96,32 +96,26 @@ class ArcReader:
             return is_periodic, unit, interval
         else:
             return False, None, None
+
     def get_cube_info(self):
+        #This function is called if `hasMultidimensions` == True
         multi_dim = get_data(
             f"{self.server_url}/multiDimensionalInfo?returnDimensionValues=always&f=pjson"
         )
-        multi_dim_variables = multi_dim.get("multidimensionalInfo", {}).get("variables", [])
-        
-        histograms = get_data(
-            f"{self.server_url}/histograms?variable=&renderingRule=&f=pjson"
-        )
-
-        statistics = get_data(
-            f"{self.server_url}/statistics?variable=&renderingRule=&f=pjson"
-        )
+        variables = multi_dim.get("multidimensionalInfo", {}).get("variables", [])
 
         cube_variables = {}
         cube_dimensions = {}
 
-        for index, variable in enumerate(multi_dim_variables):
+        for index, variable in enumerate(variables):
             var_name = variable["name"]
             
             cube_variables[var_name] = Variable(
                 {
                     "type": "data",
                     "attrs": variable.get("attributes", {}),
-                    "statistics": statistics.get('statistics', variable.get("statistics", {})),
-                    "histograms": histograms.get('histograms', variable.get("histograms", {})),
+                    "statistics": variable.get('statistics', {}),
+                    "histograms": variable.get('histograms', {}),
                     "unit": variable.get("unit", ""),
                     "dimensions": [
                         dimension["name"] for dimension in variable["dimensions"]
